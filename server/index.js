d2f4c209a1b8e27148f0ee9e5a48c4e5b74c5616
Matthew C. Rawlings <matthew.rawlings6@gmail.com>
2021-10-02 10:59:48 +0200

    First and last commit

---
+++ server/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // req.body
+
+// Routes
+
+// create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
+      [description],
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id = $2',
+      [description, id],
+    );
+
+    res.json('Todo was updated');
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// delete a todo
+app.delete('/todos/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+
+    res.json('Todo deleted');
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.listen(5000, () => {
+  console.log('server running on port 5000');
+});
